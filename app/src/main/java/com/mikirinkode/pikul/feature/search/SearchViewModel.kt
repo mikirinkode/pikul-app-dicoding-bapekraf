41e91346a3d7fe585100dbd5faad94749fe8ceaf
Muhammad Wafa <muhammadwafa105@gmail.com>
2023-07-30 14:44:05 +0700

    build search view

---
+++ app/src/main/java/com/mikirinkode/pikul/feature/search/SearchViewModel.kt
@@ -0,0 +1,31 @@
+package com.mikirinkode.pikul.feature.search
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.mikirinkode.pikul.data.local.LocalPreference
+import com.mikirinkode.pikul.data.model.PikulResult
+import com.mikirinkode.pikul.utils.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val auth: FirebaseAuth,
+    private val fireStore: FirebaseFirestore,
+    private val storage: FirebaseStorage,
+    private val preferences: LocalPreference
+) : ViewModel() {
+
+    fun getData(): MutableLiveData<PikulResult<Any>> {
+        val result = MutableLiveData<PikulResult<Any>>()
+        return result
+    }
+
+    companion object {
+        private const val TAG = "BaseViewModel"
+    }
+}
